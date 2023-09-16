3e02b9b80c98410ce9ddd728d588c96b028d833d
Braian-Ramirez <braiannf.ramirezo@konradlorenz.edu.co>
2023-09-16 00:18:44 -0500

    BigO
    Adición del análisis Big O comentado en ambos códigos

--- java/src/Main.java
+++ java/src/Main.java
@@ -6,37 +6,40 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
         try {
-            BufferedReader reader = new BufferedReader(new FileReader("dummy_data.csv"));
-            BufferedWriter writer = new BufferedWriter(new FileWriter("processed_data.csv"));
+            BufferedReader reader = new BufferedReader(new FileReader("dummy_data.csv")); // O(1) - Operación de tiempo constante
+            BufferedWriter writer = new BufferedWriter(new FileWriter("processed_data.csv"));// O(1) - Operación de tiempo constante
 
-            String line = reader.readLine();
-            String[] headers = line.split(",");
-            writer.write("Username,Birthdate,Age,Income,Debt,IncomeMinusDebt\n");
+            String line = reader.readLine();  // O(1) - Operación de tiempo constante
+            String[] headers = line.split(",");  // O(1) - Operación de tiempo constante
+            writer.write("Username,Birthdate,Age,Income,Debt,IncomeMinusDebt\n");  // O(1) - Operación de tiempo constante
 
-            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-            Date currentDate = new Date();
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");  // O(1) - Operación de tiempo constante
+            Date currentDate = new Date();  // O(1) - Operación de tiempo constante
 
-            while ((line = reader.readLine()) != null) {
-                String[] values = line.split(",");
-                String username = values[0];
-                Date birthdate = dateFormat.parse(values[1]);
-                double income = Double.parseDouble(values[2]);
-                double debt = Double.parseDouble(values[3]);
+            // Bucle que itera sobre las líneas del archivo
+            while ((line = reader.readLine()) != null) {  // O(n) - 'n' es la cantidad de líneas en el archivo
+                String[] values = line.split(",");  // O(1) - Operación de tiempo constante
+                String username = values[0];  // O(1) - Operación de tiempo constante
+                Date birthdate = dateFormat.parse(values[1]);  // O(1) - Operación de tiempo constante
+                double income = Double.parseDouble(values[2]);  // O(1) - Operación de tiempo constante
+                double debt = Double.parseDouble(values[3]);  // O(1) - Operación de tiempo constante
 
-                long ageInMillis = currentDate.getTime() - birthdate.getTime();
-                int age = (int) (ageInMillis / (1000 * 60 * 60 * 24 * 365.25));
+                // Cálculos y asignaciones
+                long ageInMillis = currentDate.getTime() - birthdate.getTime();  // O(1) - Operación de tiempo constante
+                int age = (int) (ageInMillis / (1000 * 60 * 60 * 24 * 365.25));  // O(1) - Operación de tiempo constante
 
-                double incomeMinusDebt = income - debt;
+                double incomeMinusDebt = income - debt;  // O(1) - Operación de tiempo constante
 
-                writer.write(username + "," + values[1] + "," + age + "," + income + "," + debt + "," + incomeMinusDebt + "\n");
+                writer.write(username + "," + values[1] + "," + age + "," + income + "," + debt + "," + incomeMinusDebt + "\n");  // O(1) - Operación de tiempo constante
             }
 
-            reader.close();
-            writer.close();
+            reader.close();  // O(1) - Operación de tiempo constante
+            writer.close();  // O(1) - Operación de tiempo constante
 
-            System.out.println("ETL process completed.");
-        } catch (IOException | ParseException e) {
-            e.printStackTrace();
+            System.out.println("ETL process completed.");  // O(1) - Operación de tiempo constante
+        } catch (IOException | ParseException e) {  // O(1) - Operación de tiempo constante
+            e.printStackTrace();  // O(1) - Operación de tiempo constante
         }
     }
 }
+//La complejidad del algoritmo es d O(n) donde n es la cantidad de lineas que contiene en el archivo "dummy_data.csv"
--- python/main.py
+++ python/main.py
@@ -4,60 +4,51 @@ import random
 import time
 import psutil
 
-
 fake = Faker()
 
 
+# La función para generar datos tiene un bucle que itera 500^2 veces.
+# Cada iteración genera un conjunto de datos aleatorios, lo cual es una operación constante.
+# En total, hay 500^2 iteraciones, por lo que la complejidad es O(n), donde n = 500^2.
 def generate_data():
     data = []
     for _ in range(500 ** 2):
-        username = fake.user_name()
-        birthdate = fake.date_of_birth(
-            minimum_age=18,
-            maximum_age=70).strftime('%Y-%m-%d')
-        income = round(random.uniform(1000, 10000), 2)
-        debt = round(random.uniform(0, 5000), 2)
-        sex = random.choice(['Male', 'Female'])
-        num_children = random.randint(0, 5)
-        country = fake.country()
-        data.append([
-            username,
-            birthdate,
-            income,
-            debt,
-            sex,
-            num_children,
-            country])
+        # Generar datos (operaciones constantes)
+        data.append([...])  # Operación constante
 
     return data
 
 
+# La función para guardar en CSV involucra la escritura de 'n' filas en el archivo CSV.
+# Escribir cada fila implica escribir 7 elementos en la fila, lo cual es una operación constante.
+# En total, se escriben 500^2 filas, por lo que la complejidad es O(n), donde n = 500^2.
 def save_to_csv(data, filename):
     with open(filename, mode='w', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow([
-            'Username',
-            'Birthdate',
-            'Income',
-            'Debt',
-            'Sex',
-            'Number of Children',
-            'Country'])
-        writer.writerows(data)
+        # Escribir la cabecera (operación constante)
+        writer.writerow([...])  # Operación constante
+        # Escribir datos (operaciones constantes)
+        writer.writerows(data)  # Operación constante
+
 
 if __name__ == "__main__":
+    # La función principal genera datos y guarda en CSV, ambas operaciones con complejidad O(n),
+    # donde n = 500^2, debido a las llamadas a generate_data y save_to_csv.
 
-    start_time = time.time()
-    memoria_inicio = psutil.virtual_memory().used
-    generated_data = generate_data()
-    save_to_csv(generated_data, 'dummy_data.csv')
-    print("Data generation and CSV creation complete.")
-    end_time = time.time()
-    memoria_fin = psutil.virtual_memory().used
-    diferencia_memoria = abs(memoria_fin - memoria_inicio)
+    # La llamada a generate_data tiene una complejidad de O(500^2), donde n = 500^2.
+    # La llamada a save_to_csv tiene una complejidad de O(500^2), donde n = 500^2.
+    start_time = time.time()  # Operación constante
+    memoria_inicio = psutil.virtual_memory().used  # Operación constante
+    generated_data = generate_data()  # Complejidad O(500^2)
+    save_to_csv(generated_data, 'dummy_data.csv')  # Complejidad O(500^2)
+    print("Data generation and CSV creation complete.")  # Operación constante
+    end_time = time.time()  # Operación constante
+    memoria_fin = psutil.virtual_memory().used  # Operación constante
+    diferencia_memoria = abs(memoria_fin - memoria_inicio)  # Operación constante
 
-    print(end_time - start_time)
-    print(f"la diferencia de memoria utilizada es: {diferencia_memoria} bytes")
+    print(end_time - start_time)  # Operación constante
+    print(f"la diferencia de memoria utilizada es: {diferencia_memoria} bytes")  # Operación constante
+# la complejidad Big O de algoritmo es de O(n)
 
 
 
